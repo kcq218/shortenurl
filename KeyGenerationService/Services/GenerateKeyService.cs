4b2cae0ae54969d86c1a05443be7174025541113
Kenny Quach <khangquach218@gmail.com>
2024-07-12 16:04:22 -0700

    first working version commented out commit so api wont create more keys.

---
+++ KeyGenerationService/Services/GenerateKeyService.cs
@@ -0,0 +1,49 @@
+﻿using KeyGenerationService.DAL;
+using KeyGenerationService.Models;
+
+namespace KeyGenerationService.Services
+{
+  public class GenerateKeyService : IGenerateKeyService
+  {
+    private IUnitOfWork _unitOfWork;
+    public GenerateKeyService(IUnitOfWork unitOfWork)
+    {
+      _unitOfWork = unitOfWork;
+    }
+    public void Generate(int numberOfKeys)
+    {
+        var GeneratedKeysRepository = _unitOfWork.GeneratedKeyRepository;
+
+        var seen = new HashSet<string>();
+
+        for (int i = 0; i < numberOfKeys; i++)
+        {
+            var uniqueKey = Guid.NewGuid().ToByteArray();
+            var result = Convert.ToBase64String(uniqueKey).Substring(0, 9)
+                .Replace("/", "_")
+                .Replace("+", "-");
+
+            if (!seen.Contains(result))
+            {
+                var key = new GeneratedKey();
+                key.Active = true;
+
+                key.HashValue = result;
+
+                key.CreatedBy = "droopy";
+                key.CreatedDate = DateTime.Now;
+                key.UpdatedBy = "droopy";
+                key.UpdatedDate = DateTime.Now;
+
+                GeneratedKeysRepository.Add(key);
+            }
+            else
+            {
+                seen.Add(result);
+            }
+        }
+
+        //_unitOfWork.Save();
+    }
+  }
+}
