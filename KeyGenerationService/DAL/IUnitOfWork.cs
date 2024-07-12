476a097c5b611c1ad64f8de0a4abf5320ff3b0e4
Kenny Quach <khangquach218@gmail.com>
2024-07-11 20:38:37 -0700

    unitofwork finished

---
+++ KeyGenerationService/DAL/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using KeyGenerationService.Models;
+namespace KeyGenerationService.DAL
+{
+  public interface IUnitOfWork : IDisposable
+  {
+    public IRepository<GeneratedKey> GeneratedKeyRepository { get; }
+    public IRepository<UrlMapping> UrlMappingRepository { get; }
+    public IRepository<UserInfo> UserInfoRepository { get; }
+  }
+}
--- KeyGenerationService/Models/GeneratedKey.cs
+++ KeyGenerationService/Models/GeneratedKey.cs
@@ -1,26 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace KeyGenerationService.Models;
+﻿namespace KeyGenerationService.Models;
 
 public partial class GeneratedKey
 {
-    /// <summary>
-    /// pk incremental int
-    /// </summary>
-    public int Id { get; set; }
+  /// <summary>
+  /// pk incremental int
+  /// </summary>
+  public int Id { get; set; }
 
-    public string HashValue { get; set; } = null!;
+  public string HashValue { get; set; } = null!;
 
-    public int? UrlId { get; set; }
+  public int? UrlId { get; set; }
 
-    public bool Active { get; set; }
+  public bool Active { get; set; }
 
-    public string CreatedBy { get; set; } = null!;
+  public string CreatedBy { get; set; } = null!;
 
-    public DateTime CreatedDate { get; set; }
+  public DateTime CreatedDate { get; set; }
 
-    public string UpdatedBy { get; set; } = null!;
+  public string UpdatedBy { get; set; } = null!;
 
-    public DateTime UpdatedDate { get; set; }
+  public DateTime UpdatedDate { get; set; }
 }
