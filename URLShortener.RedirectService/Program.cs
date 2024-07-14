12b9c172a97d43ce28954381999dad8bd40751a6
Kenny Quach <khangquach218@gmail.com>
2024-07-14 11:40:56 -0700

    redirect project

---
+++ URLShortener.RedirectService/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using URLShortener.DAL;
+using URLShortener.Models;
+using Microsoft.Azure.Functions.Worker;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+      services.AddApplicationInsightsTelemetryWorkerService();
+      services.ConfigureFunctionsApplicationInsights();
+      services.AddScoped<IUnitOfWork, UnitOfWork>();
+      services.AddScoped<IRepository<GeneratedKey>, Repository<GeneratedKey>>();
+      services.AddScoped<IRepository<UrlMapping>, Repository<UrlMapping>>();
+      services.AddScoped<IRepository<UserInfo>, Repository<UserInfo>>();
+    })
+    .Build();
+
+host.Run();
